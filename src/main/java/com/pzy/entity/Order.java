af0b53a6ddaab31014a1575d50c6db24928fb966
zhaoyang0501 <zhaoyang0501@126.com>
2015-09-11 15:26:25 +0800

    1

---
+++ src/main/java/com/pzy/entity/Order.java
@@ -0,0 +1,76 @@
+package com.pzy.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+@Entity
+@Table(name = "t_order")
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@ManyToOne(fetch = FetchType.EAGER)
+	private User user;
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Item item;
+	private Integer count;
+	private Double TotalPrice;
+	private Date createDate;
+	private String payType;
+	private String state;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	public Item getItem() {
+		return item;
+	}
+	public void setItem(Item item) {
+		this.item = item;
+	}
+	public Integer getCount() {
+		return count;
+	}
+	public void setCount(Integer count) {
+		this.count = count;
+	}
+	public Double getTotalPrice() {
+		return TotalPrice;
+	}
+	public void setTotalPrice(Double totalPrice) {
+		TotalPrice = totalPrice;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public String getPayType() {
+		return payType;
+	}
+	public void setPayType(String payType) {
+		this.payType = payType;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+}
---
+++ src/main/java/com/pzy/service/OrderService.java
@@ -0,0 +1,64 @@
+
+package com.pzy.service;
+
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import com.pzy.entity.Order;
+import com.pzy.repository.OrderRepository;
+
+@Service
+public class OrderService {
+	
+    @Autowired
+    private OrderRepository orderRepository;
+    public List<Order> findAll() {
+         return (List<Order>) orderRepository.findAll();
+    }
+    public List<Order> findOrderSubs() {
+        return (List<Order>) orderRepository.findAll();
+    }
+    public List<Order> findOrders() {
+        return (List<Order>) orderRepository.findAll();
+    }
+    
+    public Page<Order> findAll(final int pageNumber, final int pageSize,final String state){
+        PageRequest pageRequest = new PageRequest(pageNumber - 1, pageSize, new Sort(Direction.DESC, "id"));
+       
+        Specification<Order> spec = new Specification<Order>() {
+             public Predicate toPredicate(Root<Order> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+             Predicate predicate = cb.conjunction();
+             if (state != null) {
+                  predicate.getExpressions().add(cb.equal(root.get("state").as(String.class), state));
+             }
+             return predicate;
+             }
+        };
+        Page<Order> result = (Page<Order>) orderRepository.findAll(spec, pageRequest);
+        return result;
+    	}
+		public void delete(Long id){
+			orderRepository.delete(id);
+		}
+		public Order findOrder(Long id){
+			  return orderRepository.findOne(id);
+		}
+		public Order find(Long id){
+			  return orderRepository.findOne(id);
+		}
+		public void save(Order order){
+			orderRepository.save(order);
+		}
+}
